a4e67dcf36ee47c09424526bbfad76747d3cb20d
Tanaka <tanaka@ad.ikz-berlin.de>
2025-10-17 17:20:29 +0200

    implement temperature controller widget (minimum)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QHBoxLayout, QTabWidget
 from PyQt6.QtCore import QLocale
 from widgets.ocean_spectrometer_widget import OceanSpectrometerWidget
+from widgets.lakeshore_model335_widget import LakeShoreModel335Widget
 
 
 def main():
@@ -9,18 +10,19 @@ def main():
     QLocale.setDefault(QLocale.c())
 
     win = QWidget()
-    win.setWindowTitle("Laser Cooling App")
+    win.setWindowTitle("DLT Calibration App")
     win.resize(1200, 800)
     layout = QHBoxLayout()
 
     polling_interval = 0.5 # sec
 
     spectrometer_widget = OceanSpectrometerWidget(polling_interval=polling_interval)
-    # temperature_controller_widget = TemperatureControllerWidget(polling_interval=polling_interval)
+    temperature_controller_widget = LakeShoreModel335Widget(polling_interval=polling_interval)
 
+    spectrometer_widget.setFixedWidth(600)
 
     layout.addWidget(spectrometer_widget)
-    # layout.addWidget(temperature_controller_widget)
+    layout.addWidget(temperature_controller_widget)
     
     win.setLayout(layout)
     win.show()
---
+++ widgets/lakeshore_model335_widget.py
@@ -0,0 +1,145 @@
+from PyQt6.QtWidgets import (
+    QGroupBox, QPushButton, QLabel, QComboBox, QVBoxLayout,
+    QSpinBox, QFormLayout, QMessageBox
+)
+from PyQt6.QtCore import QThread, pyqtSignal
+import pyqtgraph as pg
+from lakeshore import Model335
+import serial.tools.list_ports
+from widgets.base_polling_thread import BasePollingThread
+import logging
+from typing import Optional
+import time
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+BAUD_RATE = 57600 # fixed baud rate for Model 335
+
+
+class LakeShoreModel335Widget(QGroupBox):
+
+    def __init__(self, parent=None, polling_interval=0.5):
+        super().__init__("Lake Shore Model335 Control", parent)
+
+        self.controller = None
+        self.polling_thread = None
+        self._polling_interval = polling_interval
+        
+        # UI Elements
+        self.scan_port_btn = QPushButton("Scan COM Port")
+        self.scan_port_btn.clicked.connect(self.scan_com_port)
+        self.ports_combo = QComboBox()
+        self.connect_btn = QPushButton("Connect")
+        self.connect_btn.clicked.connect(self.toggle_connect)
+        self.serial_number_label = QLabel("Serial Number: -----")
+
+        self.temp_A_label = QLabel("Temperature A: ----- K")
+        self.temp_B_label = QLabel("Temperature B: ----- K")
+        self.output_percentage_label = QLabel("Heater Output: -----%")
+
+        # Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.scan_port_btn)
+        layout.addWidget(self.ports_combo)
+        layout.addWidget(self.connect_btn)
+        layout.addWidget(self.serial_number_label)
+        layout.addWidget(self.temp_A_label)
+        layout.addWidget(self.temp_B_label)
+        layout.addWidget(self.output_percentage_label)
+        self.setLayout(layout)
+    
+
+    def scan_com_port(self):
+        self.ports_combo.clear()
+        ports = serial.tools.list_ports.comports()
+        for port in ports:
+            self.ports_combo.addItem(f"{port.description}", port.device)
+    
+
+    def toggle_connect(self):
+        if self.controller is None:
+            # connect
+            port = self.ports_combo.currentData()
+            if port is None:
+                QMessageBox.warning(self, "Warning", "Please selet a COM port.")
+                return
+            try:
+                self.controller = Model335(com_port=port, baud_rate=BAUD_RATE)
+            except Exception as e:
+                logging.error(f"Failed to create Lake Shore Model 335 instance: {e}")
+                return
+            try:
+                self.scan_port_btn.setEnabled(False)
+                self.ports_combo.setEnabled(False)
+                self.connect_btn.setText("Disconnect")
+                self.serial_number_label.setText(f"Serial Number: {self.controller.query("*IDN?")}")
+            except Exception as e:
+                logging.error(e)
+                return
+            # start polling
+            try:
+                self.polling_thread = LakeShoreModel335PollingThread(self.controller, self._polling_interval, parent=self)
+                self.polling_thread.updated.connect(self.update_values_display)
+                self.polling_thread.start()
+            except Exception as e:
+                logging.error(f"Failed to start polling thread: {e}")
+                return
+        else:
+            # disconnect
+            if self.polling_thread is not None:
+                self.polling_thread.stop()
+                self.polling_thread = None
+            try:
+                self.controller.disconnect_usb()
+                self.controller = None
+            except Exception as e:
+                logging.error(e)
+                return
+            self.scan_port_btn.setEnabled(True)
+            self.ports_combo.setEnabled(True)
+            self.connect_btn.setText("Connect")
+    
+
+    def update_values_display(self, data: dict):
+        temperatureA = float(data.get("temperature_A", float("nan")))
+        temperatureB = int(data.get("temperature_B", int("nan")))
+        output_percentage = float(data.get("output_percentage", float("nan")))
+        self.temp_A_label.setText(f"Temperature A: {temperatureA:.2f} K")
+        self.temp_B_label.setText(f"Temperature B: {temperatureB:.2f} K")
+        self.output_percentage_label.setText(f"Heater Output: {output_percentage}%")
+    
+
+    @property
+    def temperatures(self) -> list[float]:
+        return self._last_temp_A, self._last_temp_B
+    
+
+    def __del__(self):
+        if self.polling_thread is not None:
+            self.polling_thread.stop()
+            self.polling_thread = None
+        try:
+            self.controller.disconnect_usb()
+            self.controller = None
+        except Exception as e:
+            logging.error(e)
+
+
+# polling thread class
+class LakeShoreModel335PollingThread(BasePollingThread):
+    updated = pyqtSignal(dict)
+
+    def get_data(self) -> dict:
+        data = {
+            "temperature_A": self.controller.get_celsius_reading("A"),
+            "temperature_B": self.controller.get_celsius_reading("B"),
+            "output_percentage": self.controller.get_analog_output_percentage()
+            # "heater_status": ... ON OFF status (bool)
+        }
+        return data
+    
+
+    def emit_data(self, data: dict) -> None:
+        self.updated.emit(data)
+
+            
+            
