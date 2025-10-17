f65af8767548e8e71283fcb809554953a39b6ee2
Hiroki Tanaka <hiroki.tanaka@ikz-berlin.de>
2025-10-17 15:17:52 +0200

    communication with lakeshore temperature controller confirmed

--- lakeshore335_test.py
+++ lakeshore335_test.py
@@ -1,20 +1,18 @@
 from lakeshore import Model335
 import serial.tools.list_ports
 
-def find_model335():
+BAUD_RATE = 57600
+
+def find_lakeshore_model335():
     ports = serial.tools.list_ports.comports()
     for port in ports:
-        try:
-            if 'USB' in port.description:
-                return port.device
-        except Exception:
-            continue
-    return None
+        if "Lake Shore Model 335" in port.description:
+            return port
 
-
-com_port = find_model335()
-if not com_port:
+port = find_lakeshore_model335()
+if port is None:
     raise RuntimeError("No Lakeshore device found")
 
-temperature_controller = Model335(com_port=com_port)
+temperature_controller = Model335(com_port=port.device, baud_rate=BAUD_RATE)
 print(temperature_controller.query('*IDN?'))
+print(temperature_controller.query('*ESR?'))
